4c791585574290a0e93c0446ccb5861487dd1b23
Danisaur117 <danisaur@gmail.com>
2019-05-08 10:34:56 +0200

    Added class ContactoEntity and application.properties file

---
+++ src/main/java/com/daniel/belmonte/ContactApiRestBackend/dao/entity/ContactoEntity.java
@@ -0,0 +1,92 @@
+package com.daniel.belmonte.ContactApiRestBackend.dao.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@EntityScan
+@Table(name="contacto")
+public class ContactoEntity implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int contactoId;
+	private String nombre;
+	private String apellido1;
+	private String apellido2;
+	private int telefono;
+	private String direccion;
+	private int codigoPostal;
+	private String ciudad;
+	private String email;
+	private Date lastUpdate;
+	public int getContactoId() {
+		return contactoId;
+	}
+	public void setContactoId(int contactoId) {
+		this.contactoId = contactoId;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido1() {
+		return apellido1;
+	}
+	public void setApellido1(String apellido1) {
+		this.apellido1 = apellido1;
+	}
+	public String getApellido2() {
+		return apellido2;
+	}
+	public void setApellido2(String apellido2) {
+		this.apellido2 = apellido2;
+	}
+	public int getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public int getCodigoPostal() {
+		return codigoPostal;
+	}
+	public void setCodigoPostal(int codigoPostal) {
+		this.codigoPostal = codigoPostal;
+	}
+	public String getCiudad() {
+		return ciudad;
+	}
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getLastUpdate() {
+		return lastUpdate;
+	}
+	public void setLastUpdate(Date lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+}
